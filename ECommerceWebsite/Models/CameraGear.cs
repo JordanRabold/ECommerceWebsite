29eabcce849814cb3c0e89e6f6d7fb2cc3ccaefa
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-01 09:27:08 -0700

    Added Camera Gear class

---
+++ ECommerceWebsite/Models/CameraGear.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceWebsite.Models
+{
+    /// <summary>
+    /// Represents a single piece of Camera Gear for purchase
+    /// </summary>
+    public class CameraGear
+    {
+        /// <summary>
+        /// primary key for each camera gear product
+        /// </summary>
+        [Key]
+        public int CameraGearId { get; set; }
+
+        /// <summary>
+        /// The title of the product
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The sale price of the product
+        /// </summary>
+        [Range(0, 10000)]
+        public double Price { get; set; }
+
+        // Todo: Add rating
+    }
+}
