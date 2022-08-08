789c704f65861b1f0f9d9c5af7b9ed1f977ba0bc
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-08 09:38:47 -0700

    Create cameragear fully functional, added styling

--- ECommerceWebsite/Controllers/CameraGearController.cs
+++ ECommerceWebsite/Controllers/CameraGearController.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceWebsite.Data;
+using ECommerceWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceWebsite.Controllers
-{
+{ 
     public class CameraGearController : Controller
     {
+        private readonly CameraGearContext _context;
+
+        public CameraGearController(CameraGearContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CameraGear camGear)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to DB
+                _context.cameraGears.Add(camGear); // prepares insert
+                _context.SaveChanges(); // Executes pending insert
+
+                // Show success message on page
+                ViewData["Message"] = $"{camGear.Title} was added successfully!";
+
+                return View();
+            }
+
+            return View(camGear);
         }
     }
 }
