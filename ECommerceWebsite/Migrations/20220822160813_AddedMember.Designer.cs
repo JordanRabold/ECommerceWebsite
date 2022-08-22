6f96b9388749cdf37f2c786568bd84e873754b8b
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-22 09:10:52 -0700

    Added migration for member class

--- ECommerceWebsite/Migrations/20220822160813_AddedMember.Designer.cs
+++ ECommerceWebsite/Migrations/20220822160813_AddedMember.Designer.cs
@@ -3,6 +3,7 @@ using ECommerceWebsite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerceWebsite.Migrations
 {
     [DbContext(typeof(CameraGearContext))]
-    partial class CameraGearContextModelSnapshot : ModelSnapshot
+    [Migration("20220822160813_AddedMember")]
+    partial class AddedMember
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
