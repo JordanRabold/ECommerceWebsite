33c1bf9624c94608125ebde4116e40627bb01ca1
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-22 09:31:57 -0700

    Added Register ViewModel

--- ECommerceWebsite/Models/Member.cs
+++ ECommerceWebsite/Models/Member.cs
@@ -7,15 +7,36 @@ namespace ECommerceWebsite.Models
         [Key]
         public int MemberId { get; set; }
 
-        [Required]
         public string Email { get; set; }
 
-        [Required]
         public string Password { get; set; }
 
         public string PhoneNumber { get; set; }
 
         public string UserName { get; set; }
+
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 10)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
 
     }
 }
