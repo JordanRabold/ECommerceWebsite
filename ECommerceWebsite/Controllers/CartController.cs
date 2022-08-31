ec83e4b2821eac795cce6ed715765a124d55e70c
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-31 08:57:38 -0700

    Add shopping cart button to camer gear catalog

---
+++ ECommerceWebsite/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerceWebsite.Data;
+using ECommerceWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceWebsite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CameraGearContext _context;
+
+        public CartController(CameraGearContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            CameraGear gearToAdd = _context.cameraGears.Where(c => c.CameraGearId == id).SingleOrDefault();
+
+            if(gearToAdd == null)
+            {
+                // Gear with that id doesn't exist
+                TempData["Message"] = "Sorry, that gear no longer exists";
+                return RedirectToAction("Index", "CameraGear");
+            }
+
+            // Todo: Add item to a shopping cart cookie
+            TempData["Message"] = "Item added to cart";
+            return RedirectToAction("Index", "CameraGear");
+        }
+    }
+}
